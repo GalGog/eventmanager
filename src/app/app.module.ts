74469112fee0b8ec79923d065585fa4e5925e7fa
GalGog <galynna00@gmail.com>
2018-02-12 12:36:22 +0000

    testing eventmanager

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { FormioAppConfig } from 'angular-formio';
+import { FormioAuthService, FormioAuthConfig } from 'angular-formio/auth';
+import { AuthConfig, AppConfig } from '../config';
+import { AuthModule } from './auth/auth.module';
+
 
 
 import { AppComponent } from './app.component';
@@ -12,9 +19,28 @@ import { HomeComponent } from './home/home.component';
     HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full'
+      },
+      {
+        path: 'home',
+        component: HomeComponent
+      },
+      {
+        path: 'auth',
+        loadChildren: () => AuthModule
+      }
+    ])
   ],
-  providers: [],
+  providers: [
+  FormioAuthService,
+  {provide: FormioAuthConfig, useValue: AuthConfig},
+  {provide: FormioAppConfig, useValue: AppConfig}
+],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
