473680147727d9003f0b867c73632e8eefcf81b1
GalGog <galynna00@gmail.com>
2018-02-13 11:42:36 +0000

    testing eventmanager

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
-import { FormioAppConfig } from 'angular-formio';
-import { FormioAuthService, FormioAuthConfig } from 'angular-formio/auth';
-import { AuthConfig, AppConfig } from '../config';
-import { AuthModule } from './auth/auth.module';
+
 
 
 
